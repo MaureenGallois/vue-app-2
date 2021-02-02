111c9041d4a27e2e63fd14eb139fe50433fe3830
Maureen GALLOIS <m.gallois@unyc.io>
2021-02-02 14:09:51 +0100

    mise en place de lapplication

---
+++ docs/precache-manifest.c57b5ec18079fa646cdac25a29973f8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63b25d795ae35972888",
+    "url": "/vue-app-2/css/app.00931879.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a007549204ba1292a20069b1a029c66d",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "967423926572a8ccf216",
+    "url": "/vue-app-2/js/about.31fb3868.js"
+  },
+  {
+    "revision": "e63b25d795ae35972888",
+    "url": "/vue-app-2/js/app.1dff6619.js"
+  },
+  {
+    "revision": "ba2a6fda9ddaaddeab4b",
+    "url": "/vue-app-2/js/chunk-vendors.f449932c.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
